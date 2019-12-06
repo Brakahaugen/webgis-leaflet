550c5f66e06bf29c7a290935a129c381ec33faa6
Brakahaugen <espenbragerhaug@gmail.com>
2019-12-06 10:07:32 +0100

    Updates

---
+++ precache-manifest.303b7eb32979d7f13d63505092134687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d530559653029f2c6275880e934cb4",
+    "url": "/webgis-leaflet/index.html"
+  },
+  {
+    "revision": "15f9c7e79a989ba4681c",
+    "url": "/webgis-leaflet/static/css/2.847213c2.chunk.css"
+  },
+  {
+    "revision": "06ff54de9f2da177cbf8",
+    "url": "/webgis-leaflet/static/css/main.2129e47f.chunk.css"
+  },
+  {
+    "revision": "15f9c7e79a989ba4681c",
+    "url": "/webgis-leaflet/static/js/2.5dbe5a62.chunk.js"
+  },
+  {
+    "revision": "06ff54de9f2da177cbf8",
+    "url": "/webgis-leaflet/static/js/main.fd4029f2.chunk.js"
+  },
+  {
+    "revision": "6281b9f709c0b964e76b",
+    "url": "/webgis-leaflet/static/js/runtime-main.4c24673b.js"
+  }
+]);
